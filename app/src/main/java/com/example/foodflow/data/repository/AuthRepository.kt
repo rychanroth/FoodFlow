60cf698e150b260f5ffe6a96a5e053154f0dd25c
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-19 15:51:06 +0700

    feat(repository): add `registerUser`

--- app/src/main/java/com/example/foodflow/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/foodflow/data/repository/AuthRepository.kt
@@ -1,4 +1,26 @@
 package com.example.foodflow.data.repository
 
+import com.example.foodflow.data.model.AppUser
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
 class AuthRepository {
+    private val auth = FirebaseAuth.getInstance()
+    private val firestore = FirebaseFirestore.getInstance()
+
+    suspend fun registerUser(email: String, password: String, role: String): Result<AppUser> {
+        return try {
+            val authResult = auth.createUserWithEmailAndPassword(email, password).await()
+            val uid = authResult?.user?.uid ?: throw Exception("User creation failed!")
+
+            val newUser = AppUser(uid, email, role)
+
+            firestore.collection("users").document(uid).set(newUser).await()
+
+            Result.success(newUser)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
 }
