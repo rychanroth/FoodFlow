f21582bfd265ac4173bb6422aae28702dc5b7543
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-22 18:15:50 +0700

    chore: add dependecy for `okhttp`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,5 +67,5 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
-    implementation("com.google.firebase:firebase-storage")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
