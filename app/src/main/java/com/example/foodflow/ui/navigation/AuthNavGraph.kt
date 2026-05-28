6201a94eba33807ddc34b5bbe270cf8c9affd999
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-28 16:15:41 +0700

    feat(nagivation): add `AuthNavGraph`

---
+++ app/src/main/java/com/example/foodflow/ui/navigation/AuthNavGraph.kt
@@ -0,0 +1,22 @@
+package com.example.foodflow.ui.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.example.foodflow.ui.Route
+import com.example.foodflow.ui.screens.auth.ForgotPasswordScreen
+import com.example.foodflow.ui.screens.auth.LoginScreen
+import com.example.foodflow.ui.screens.auth.RegisterScreen
+import com.example.foodflow.ui.viewmodel.AuthViewModel
+
+fun NavGraphBuilder.authGraph(navController: NavController, authViewModel: AuthViewModel) {
+    composable(Route.Login.route) {
+        LoginScreen(navController, authViewModel)
+    }
+    composable(Route.Register.route) {
+        RegisterScreen(navController, authViewModel)
+    }
+    composable(Route.ForgotPassword.route) {
+        ForgotPasswordScreen(navController, authViewModel)
+    }
+}
