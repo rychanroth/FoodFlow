bed806aab5029da3a208338db0f88dfe5955b5e2
Ry Chanroth <rychanroth2009@gmail.com>
2026-06-07 17:50:13 +0700

    feat: renovate ui for `ApplicationScreen`

--- app/src/main/java/com/example/foodflow/ui/navigation/AdminNavGraph.kt
+++ app/src/main/java/com/example/foodflow/ui/navigation/AdminNavGraph.kt
@@ -4,7 +4,6 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
-import com.example.foodflow.ui.navigation.Route
 import com.example.foodflow.ui.screens.admin.ApplicationScreen
 import com.example.foodflow.ui.screens.admin.DashboardScreen
 import com.example.foodflow.ui.screens.admin.SettingsScreen
@@ -25,7 +24,7 @@ fun NavGraphBuilder.adminGraph(
             DashboardScreen(authViewModel)
         }
         composable(Route.AdminApplications.route) {
-            ApplicationScreen()
+            ApplicationScreen(navController)
         }
         composable(Route.AdminSettings.route) {
             SettingsScreen()
